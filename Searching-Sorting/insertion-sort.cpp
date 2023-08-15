86e3906c6adea1a29ee03d6175e8ff9b0fabad86
Parth Bhattacharya <parthget@gmail.com>
2023-08-15 20:21:20 +0530

    add insertion sort problem

---
+++ Searching-Sorting/insertion-sort.cpp
@@ -0,0 +1,44 @@
+// Insertion sort
+// we start from i+1 index, then compare with 0 to i+1 elements
+// then we shift elements bigger than current element to i+1
+// then we copy the value of current element to correct index
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> arr{10, 1, 7, 6, 14, 9};
+    int n = arr.size();
+
+    // iterate through elements(starting from round 1)
+    for (int round = 1; round < n; round++)
+    {
+        // fetch
+        int val = arr[round];
+        int j = round - 1;
+        // compare
+        for (j; j >= 0; j--)
+        {
+            if (arr[j] > val)
+            {
+                // shift
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                // exit loop
+                break;
+            }
+        }
+        // copy
+        arr[j + 1] = val; // as loop will exit after checking condition and perform an extra j-1
+    }
+
+    // print sorted array
+    cout << "sorted array is " << endl;
+    for (auto val : arr)
+    {
+        cout << val << " ";
+    }
+}
