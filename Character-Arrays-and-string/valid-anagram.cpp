f030b089a805bcfcdefa1609b4144ba271611091
Parth Bhattacharya <parthget@gmail.com>
2023-08-23 20:24:33 +0530

    updates valid-anagram problem

--- Character-Arrays-and-string/valid-anagram.cpp
+++ Character-Arrays-and-string/valid-anagram.cpp
@@ -24,19 +24,52 @@
 #include <algorithm>
 using namespace std;
 
+// BRUTE FORCE -> T.C : )(nlogn)
+// bool isAnagram(string s, string t)
+// {
+//     sort(s.begin(), s.end());
+//     sort(t.begin(), t.end());
+
+//     if (s == t)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+
+// BETTER APPROACH -> Frequency Table for Strings
+
+// TOTAL ASCII = 256
 bool isAnagram(string s, string t)
 {
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
+    // since ACII characters lie from 0 to 256
+    // we can create an array of length 256 and initialize it with 0 (all 0 elements)
+    // indexes from 0 to 256 represent ASCII values
+    int freqTable[256] = {0};
 
-    if (s == t)
+    // Our approach invloves looping through the string and increasing count
+    // for respective ASCII values as they occur in the first string
+    for (int i = 0; i < s.length(); i++)
     {
-        return true;
+        freqTable[s[i]]++;
     }
-    else
+
+    // Now we can decrease count of the ACII values for the second string
+    for (int i = 0; i < t.length(); i++)
     {
-        return false;
+        freqTable[s[i]]--;
     }
+
+    // If the strings are anagram, all values in the array will be reset to 0
+    for (int i = 0; i < 256; i++)
+    {
+        if (freqTable[i] != 0)
+            return false;
+    }
+    return true;
 }
 
 int main()
@@ -49,7 +82,14 @@ int main()
     cin >> t;
 
     // OUTPUT
-    cout << isAnagram(s, t);
+    if (isAnagram(s, t))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
 
     return 0;
 }
