4e19d0edfa8bb1f98634e44139fa655ca6977d34
Parth Bhattacharya <parthget@gmail.com>
2023-08-29 11:10:04 +0530

    add string-compression-problem

--- Character-Arrays-and-string/string-compression.cpp
+++ Character-Arrays-and-string/string-compression.cpp
@@ -42,15 +42,57 @@
 #include <algorithm>
 using namespace std;
 
-int compress(vector<char> &chars)
+int compress(vector<char> &s)
 {
-    return 0;
+    int index = 0, count = 1;
+    char prev = s[0];
+
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] == prev)
+        {
+            count++;
+        }
+        else
+        {
+            s[index++] = prev;
+            if (count > 1)
+            {
+                int start = index;
+                while (count)
+                {
+                    s[index++] = (count % 10) + '0';
+                    count /= 10;
+                }
+                reverse(s.begin() + start, s.begin() + index);
+            }
+            prev = s[i];
+            count = 1;
+        }
+    }
+    s[index++] = prev;
+    if (count > 1)
+    {
+        int start = index;
+        while (count)
+        {
+            s[index++] = (count % 10) + '0';
+            count /= 10;
+        }
+        reverse(s.begin() + start, s.begin() + index);
+    }
+    return index;
 }
 
 int main()
 {
-    vector<char> chars{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+    vector<char> s{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+    cout << compress(s) << endl;
 
-    cout << compress(chars) << endl;
+    for (auto ch : s)
+    {
+        cout << ch << " ";
+    }
+    cout << endl;
     return 0;
 }
