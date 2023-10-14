c55d6ef51c2fac9836131370cb66beca37572c12
Parth Bhattacharya <parthget@gmail.com>
2023-10-14 11:20:15 +0530

    add to arrays problems js

--- Arrays/js/sort0and1.js
+++ Arrays/js/sort0and1.js
@@ -1,5 +1,33 @@
 // sort 0's and 1's
 
+// count number of occurences of 0 and 1
+// const sortFunc = (arr) => {
+//   let zeroCount = 0;
+//   let oneCount = 0;
+
+//   //count zeros and ones
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 0) {
+//       zeroCount++;
+//     } else {
+//       oneCount++;
+//     }
+//   }
+//   //fill zeros and ones
+//   for (let i = 0; i < arr.length; i++) {
+//     while (zeroCount--) {
+//       arr[j] = 0;
+//       j++;
+//     }
+//     if (i > zeroCount) {
+//       arr[i] = 1;
+//     }
+//   }
+//   return arr;
+// };
+
+// Two pointer approach
+
 const sortFunc = (arr) => {
   let start = 0;
   let end = arr.length - 1;
@@ -25,6 +53,6 @@ const sortFunc = (arr) => {
 };
 
 //output
-const arr = [1, 1, 0];
+const arr = [0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0];
 
 console.log(sortFunc(arr));
