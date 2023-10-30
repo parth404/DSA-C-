5596f50150fc73cb56c0077d9e934ea75f4de1f8
Parth Bhattacharya <parthget@gmail.com>
2023-10-30 12:03:11 +0530

    add to topic hoisting

--- javascript/hoisting/examples.js
+++ javascript/hoisting/examples.js
@@ -29,3 +29,89 @@ function greet() {
 //   };
 // in memory execution context, greet would have been undefined and we would get
 // greet is not a function as output
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Temporal Dead Zone
+
+// Example
+// console.log(a);
+// let a = 10;
+// var b = 100;
+
+// Uncaught ReferenceError: Cannot access 'a' before initialization
+
+// To understand this behavior better we can see the scopes inn which these variables are assigned using debugger tools
+
+// adding a break point at let a = 10. i.e to observe the memory creation phase
+// shows that var b is assigned to global scope where as let a is in script
+
+// Script
+// a : <value unavailable></value>
+
+// Global
+// b : undefined
+
+// forwarding one step in debugger, let's us observe that a is now assigned the value
+// as the line var b = 100 has not been read yet b is still undefined
+
+// Script
+// a : 10
+// Global
+// b : undefined
+
+// when var b = 100 is read, we can observe that b is assigned a vlaue of 100
+// Global
+// b : 100
+
+// Temporal Dead Zone: The time since when let variable was hoisted and uptill it was assigned a value
+
+// so in case we had our order as below
+// console.log(a); // this line gets executed when a is still in temporal dead zone
+// let a = 10;
+// var b = 100;
+
+// hence when we try to access a before it exits the temporal dead zone it gives us a Uncaught ReferenceError
+
+// Difference between in var, let and const
+
+// let a = 10;
+// console.log(a);
+// let a = 20;
+
+// Doing the above would throw an error
+// Uncaught SyntaxError: Identifier 'a' has already been declared
+
+// However --
+// var a = 10;
+// console.log(a);
+// var a = 20;
+
+// output 10
+
+// This doesnot throw any error
+
+//------------------------------When using Const--------------------
+// let a;
+// console.log(a);
+// a = 20;
+// output undefined -- however no error is thrown
+
+// however for const
+
+// const a;
+// console.log(a);
+// a = 20;
+
+// Uncaught SyntaxError: Missing initializer in const declaration. So, wehen using cont we need to initialize with value when using const to declare a variable
+
+// var b = 20;
+// let b = 30;
+
+// console.log(b); // Uncaught SyntaxError: Identifier 'b' has already been declared
+
+// also
+
+// const c = 1000;
+
+// c = 30; // Uncaught TypeError: Assignment to constant variable.
